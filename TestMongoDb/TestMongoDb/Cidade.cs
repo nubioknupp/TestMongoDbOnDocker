a50d8fa9ca63b08d5b7953bbfc2610a30e9556b0
Núbio Knupp <nubioknupp@gmail.com>
2017-02-12 16:55:13 -0200

    Teste na conexão com MongoDB em uma Docker  via C#

---
+++ TestMongoDb/TestMongoDb/Cidade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MongoDB.Bson;
+
+namespace TestMongoDb
+{
+	public class Cidade
+	{
+		public ObjectId _id { get; set; }
+		public string Nome { get; set; }
+		public string Estado { get; set; }
+		public string Pais { get; set; }
+	}
+}
---
+++ TestMongoDb/TestMongoDb/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MongoDB.Driver;
+
+namespace TestMongoDb
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			string connectionString = "mongodb://192.168.99.100:32769";
+
+			var client = new MongoClient(connectionString);
+			var db = client.GetDatabase("TestMongoDb");
+
+			var cidades = db.GetCollection<Cidade>("cidades");
+
+			var cidade = new Cidade();
+			cidade.Nome = "Juiz de Fora";
+			cidade.Estado = "Minas Gerais";
+			cidade.Pais = "Brasil";
+
+			cidades.InsertOne(cidade);
+
+			Console.WriteLine(cidade._id);
+
+		}
+	}
+}
